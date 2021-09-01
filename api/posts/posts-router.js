df5a2227fe28bf57235f860a8f9653da31f38083
Andrew Cummings <andrew.amalee@gmail.com>
2021-08-31 19:31:07 -0700

    16/17 passing

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -38,9 +38,12 @@ router.post('/api/posts', (req, res) => {
         res.status(400).json({ message: "Please provide title and contents for the post" })
     } else {
         Posts.insert(newPost)
+        .then(({ id }) => {
+            return Posts.findById(id)
+        })
         .then((post) => {
             res.status(201).json(post)
-            return post
+            
         })
         .catch(error => {
             console.log(error)
@@ -61,10 +64,45 @@ router.put('/api/posts/:id', async (req, res) => {
     } else {
         const result = await Posts.update(id, changes)
         res.status(200).json(result)
+        console.log(id)
+        
     }
-} catch (err) {
+        } catch (err) {
     res.status(500).json({ message: "The post information could not be modified" })
 }
 })
+
+router.delete('/api/posts/:id', async (req, res) => {
+    try {
+    const post = await Posts.findById(req.params.id)
+            if(!post) {
+                res.status(404).json({ message: "The post with the specified ID does not exist" })
+            } else {
+                await Posts.remove(req.params.id)
+                res.status(200).json(post)
+            }
+        }
+    
+        catch(error) {
+            res.status(500).json({ message: "The comments information could not be retrieved" })
+        }
+})
+
+router.get('/api/posts/:id/comments', async (req, res) => {
+   try {
+    const userPost = await Posts.findById(req.params.id)
+    if (!userPost) {
+        res.status(404).json({ message: "The post with the specified ID does not exist" })
+    } else {
+        const messages = await Posts.findPostComments(req.params.id)
+            res.status(200).json(messages)
+        }
+    }
+        catch(error) {
+            console.log(error)
+            res.status(500).json({ message: "The comments information could not be retrieved" })
+        }
+    })
+
 
 module.exports = router
